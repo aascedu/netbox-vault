22caa077763e1fd75ff03e115126ad1256499175
IPLine <ipline@P107728-WebApp-Backup>
2024-10-23 15:40:43 +0200

    wrongm name :)

--- netbox_vault/models/vault.py
+++ netbox_vault/models/vault.py
@@ -2,7 +2,7 @@ from django.db import models
 from netbox.models import NetBoxModel
 
 class Vault(NetBoxModel):
-	vault_name = models.CharField(max_length=20)
+	name = models.CharField(max_length=20)
 
 	class Meta:
     	ordering = ('name',)
